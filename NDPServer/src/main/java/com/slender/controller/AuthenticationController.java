03d06e47cd44ed26cdad1c8a9c2aae17edabbae2
slender <2037779479@qq.com>
2025-12-05 15:32:33 +0800

    用户CRUD完成

--- NDPServer/src/main/java/com/slender/controller/AuthenticationController.java
+++ NDPServer/src/main/java/com/slender/controller/AuthenticationController.java
@@ -1,20 +1,17 @@
 package com.slender.controller;
 
+import com.slender.annotation.FakeInterface;
 import com.slender.dto.authentication.CaptchaRequest;
 import com.slender.dto.authentication.LoginByCaptchaRequest;
 import com.slender.dto.authentication.LoginByPasswordRequest;
 import com.slender.dto.user.UserRegisterRequest;
-import com.slender.dto.user.UserResetRequest;
-import com.slender.entity.User;
 import com.slender.result.Response;
 import com.slender.service.interfase.UserService;
 import com.slender.vo.LoginData;
-import com.slender.vo.RefreshData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,12 +22,15 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "访问控制",description = "用户登录登出刷新接口")
 public class AuthenticationController {
     private final UserService userService;
+
+    @FakeInterface
     @PostMapping("/login/password")
     @Operation(summary = "密码登录")
     public Response<LoginData> loginByPassword(@RequestBody LoginByPasswordRequest request){
         return null;
     }
 
+    @FakeInterface
     @PostMapping("/login/captcha")
     @Operation(summary = "验证码登录")
     public Response<LoginData> loginByCaptcha(@RequestBody LoginByCaptchaRequest request){
@@ -39,7 +39,7 @@ public class AuthenticationController {
 
     @PostMapping("/captcha")
     @Operation(summary = "验证码获取")
-    public Response<Void> loginByCaptcha(@RequestBody @Validated CaptchaRequest captchaRequest){
+    public Response<Void> getCaptcha(@RequestBody @Validated CaptchaRequest captchaRequest){
         userService.sendCaptcha(captchaRequest);
         return Response.success("验证码发送成功");
     }
@@ -51,17 +51,4 @@ public class AuthenticationController {
         return Response.success("注册成功");
     }
 
-    @PostMapping("/reset")
-    @Operation(summary = "重置密码")
-    public Response<Void> reset(@RequestBody @Validated UserResetRequest userResetRequest, @AuthenticationPrincipal User user){
-        return userService.reset(userResetRequest,user) ?
-                Response.success("重置密码成功") :
-                Response.fail("重置密码失败");
-    }
-
-    @GetMapping("/refresh")
-    @Operation(summary = "刷新token")
-    public Response<RefreshData> refresh(@AuthenticationPrincipal Long uid){
-        return Response.success("token刷新成功",userService.refresh(uid));
-    }
 }
